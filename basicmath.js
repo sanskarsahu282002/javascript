8cfb1a1b86505ea28c0804eb4653a61f1b1b3b96
sanskarsahu282002 <116749721+sanskarsahu282002@users.noreply.github.com>
2023-09-26 02:46:53 +0530

    Add files via upload

---
+++ basicmath.js
@@ -0,0 +1,40 @@
+// case sensitive
+// declaration
+var sanskarSahu
+var honeyCake
+var amanRaj
+// intialisation
+ sanskarSahu=100;
+ honeyCake=39;
+ amanRaj=90;
+// adding two numbers
+var sum=10+10;
+console.log(sum)
+// subtratic  two  numbers
+var sub=90000-34;
+console.log(sub)
+// dividing two  numbers
+var div=67/9;
+console.log(div)
+//incrementing a number
+var add=999;
+add++;
+++add
+console.log(add)
+var dec=6777;
+//dec--;
+--dec;
+console.log(dec)
+var product=5.6*8.9
+console.log(product)
+var remainder=4%6
+console.log(remainder)
+var rem=6%4
+console.log(rem)
+var a=7
+var b=9
+var c=7
+a+=12
+b+=6
+c+=7
+console.log(a)
